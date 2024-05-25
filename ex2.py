9848698fa1fc459e4ab38af8b49d9e5409c2e329
queso-gato1355 <solar767@unist.ac.kr>
2024-05-25 18:55:30 +0900

    :hammer: Add example files.

---
+++ ex2.py
@@ -0,0 +1,11 @@
+import json
+from pykma.KMA import KMA
+from pykma.links import *
+
+myKma = KMA("-s73Jz7MQTiO9yc-zCE4xw")
+
+typ01_to_json = myKma.typ01_request(ASOS_SPECIFIC_TIME_REQUEST)
+
+# 파일에 작성
+with open("station_info2.json", "w", encoding='utf-8') as f:
+    f.write(json.dumps(typ01_to_json.to_json(), indent=4, ensure_ascii=False))
